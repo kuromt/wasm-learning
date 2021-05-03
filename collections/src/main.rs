8627504701eb66495a5b7105777dc2f494b57a17
黒松信行 <kuromt@bookers.local>
2021-05-03 14:11:01 +0900

    add collections

--- collections/src/main.rs
+++ collections/src/main.rs
@@ -1,7 +1,16 @@
+use std::collections::HashMap;
+use std::time::Instant;
+
 fn main() {
 
-    for c in "नमस्ते".bytes() {
-        println!("{}", c);
+    let text = "hello world wonderful world";
+
+    let mut map = HashMap::new();
+    
+    for word in text.split_whitespace() {
+        let count = map.entry(word).or_insert(0);
+        *count += 1;
     }
 
+    println!("{:?}", map);
 }
